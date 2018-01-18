4374f5f617f18bab2f45810834a5f5e082412174
Kristie Huang <kristiehuang01@gmail.com>
2018-01-18 11:50:36 -0800

    gridworld setup

--- Mobile2/src/Weight.java
+++ Mobile2/src/Weight.java
@@ -3,11 +3,11 @@
 // If you make Structure a class, Weight must extend it.
 // If you make Structure an interface, Weight must implement it.
 
-public class Weight extends Structure {
-    // This constructor is now mandatory as we can make weights
-    // real objects instead of treating them solely as numbers.
-    // 
-    // You have to figure out how to handle it
+public class Weight extends Structure { 
+	
+	
+	//extends = IS A class
+	//implements = HAS AN interface
 
     public Weight(double weight) {
     		super(weight);
