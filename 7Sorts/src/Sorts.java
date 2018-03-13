a4d50f9d289a38efd2882c4db6d60421915ce9cd
Kristie Huang <kristiehuang01@gmail.com>
2018-03-13 11:58:42 -0700

    3/31- arjun's median

--- 7Sorts/src/Sorts.java
+++ 7Sorts/src/Sorts.java
@@ -115,147 +115,116 @@ public class Sorts {
 
 	}
 	//	// QUICK SORT
-	//
-	//
-	//	void quicksort(int[] a) {
-	//		quick(a, 0, a.length-1);
-	//	}
-	//
-	//	void quick(int[] a, int left, int right) {
-	//		if (right > left) { //if last index is bigger than 0
-	//			int pivotPos = partition(a, left, right);
-	//
-	//			quick(a, left, pivotPos-1);
-	//			quick(a, pivotPos+1, right);
-	//		}
-	//	}
-	//
-	//	static int partition(int[] a, int left, int right) {
-	//		int splitPos = left; //1
-	//		for (int i = left; i < right; i++) { //index from 0
-	//			if (a[i] < a[right]) {
-	//				swap(a, i, splitPos);
-	//				splitPos++;
-	//			}
-	//		}
-	//		swap(a,splitPos,right);
-	//		return splitPos;
-	//	}
-	//
-	//	void swap(int[] a, int i, int j) {
-	//		int temp = a[i];
-	//		a[i] = a[j];
-	//		a[j] = temp;
-	//	}
-	//
-	//
-	//
-	//	public static double median(int[] a) {
-	//		double median = medianQuick(a, 0, a.length-1);
-	//
-	//		System.out.println("median " + median);
-	//		return median;
-	//	}
-	//
-	//	static double medianQuick(int[] a, int left, int right) {
-	//
-	//		if (right > left) {
-	//			int pivotPos = partition2(a, left, right);
-	//			int middle = a.length/2;
-	//
-	//			if (a.length % 2 == 1) { //if odd, 1 middle
-	//				if (pivotPos == middle) return (a[middle]);
-	//			} 
-	//
-	//
-	//			else {  // Even number of elements, 2 middles
-	//				// pivotPos == middle looks like xxxxxPxxxx
-	//				if (pivotPos == middle) {
-	//					if (middle == left) return a[middle];
-	//					else return (a[middle] + select(a, left, middle-1, 1)) / 2.0;
-	//
-	//					// pivotPos == middle - 1 looks like xxxxPxxxxx
-	//				} else if (pivotPos == middle - 1) {
-	//					if (middle == right) return a[middle];
-	//					else return (a[middle-1] + select(a, middle, right, 0)) / 2.0;
-	//				}
-	//			}
-	//
-	//
-	//			//if pivot pos is NOT = middle, so we sort left/right
-	//
-	//			if (pivotPos < middle) {
-	//				return medianQuick2(a, pivotPos+1, right);
-	//			} else {
-	//				return medianQuick2(a, left, pivotPos-1);
-	//			}
-	//			// If we ever get to a situation where we are "sorting" one element,
-	//			// it must be a middle element
-	//		} else {
-	//			return a[left];
-	//		}
-	//	}
-	//
-	//
-	//	//teacher solution
-	//	public static double medianQuick2(int[] a, int left, int right) {
-	//		if (right > left) {
-	//			int pivotPos = partition2(a, left, right);
-	//			int middle = a.length/2;
-	//
-	//
-	//			if (a.length % 2 == 1) { //odd length
-	//				if (pivotPos == middle) return (a[middle]);
-	//			} else {  // Even number of elements
-	//				// Once one of the two middle indices has been found, borrow from
-	//				// selection sort and select either the minimum value from the right
-	//				// side or the maximum value from the left side, depending on which side
-	//				// does not contain the found middle index.
-	//				//
-	//				// pivotPos == middle looks like xxxxxPxxxx
-	//				if (pivotPos == middle) {
-	//					if (middle == left) return a[middle];
-	//					else return (a[middle] + select(a, left, middle-1, 1)) / 2.0;
-	//
-	//					// pivotPos == middle - 1 looks like xxxxPxxxxx
-	//				} else if (pivotPos == middle - 1) {
-	//					if (middle == right) return a[middle];
-	//					else return (a[middle-1] + select(a, middle, right, 0)) / 2.0;
-	//				}
-	//			}
-	//
-	//			// Here is where we make sure to "sort" the elements that move us
-	//			// towards the median element(s)
-	//			if (pivotPos < middle) {
-	//				return medianQuick2(a, pivotPos+1, right);
-	//			} else {
-	//				return medianQuick2(a, left, pivotPos-1);
-	//			}
-	//			// If we ever get to a situation where we are "sorting" one element,
-	//			// it must be a middle element
-	//		} else {
-	//			return a[left];
-	//		}
-	//	}
-	//
-	//
-	//	static int partition2(int[] a, int left, int right) {
-	//		int splitPos = left;  //0
-	//		for (int i = left; i < right; i++) {  //i = 0, right = last index
-	//			if (a[i] < a[right]) {
-	//				swap2(a, i, splitPos);
-	//				splitPos++;
-	//			}
-	//		}
-	//		swap2(a,splitPos,right);
-	//		return splitPos;
-	//	}
-	//
-	//	static void swap2(int[] a, int i, int j) {
-	//		int temp = a[i];
-	//		a[i] = a[j];
-	//		a[j] = temp;
-	//	}
+
+
+	public void quicksort(int[] a) {
+		quick(a, 0, a.length-1);
+	}
+
+	public void quick(int[] a, int left, int right) {
+		if (right > left) { //if last index is bigger than 0
+			int pivotPos = partition(a, left, right);
+
+			quick(a, left, pivotPos-1);
+			quick(a, pivotPos+1, right);
+		}
+	}
+
+	public int partition(int[] a, int left, int right) {
+		int splitPos = left; //1
+		for (int i = left; i < right; i++) { //index from 0
+			if (a[i] < a[right]) {
+				swap(a, i, splitPos);
+				splitPos++;
+			}
+		}
+		swap(a,splitPos,right);
+		return splitPos;
+	}
+
+	public void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+
+
+	public static double median(int[] a) {
+		return median(a, 0, a.length-1);
+	}
+
+	public static double median(int[] a, int left, int right) {
+		int pivotPos = partition2(a, left, right);
+		int middle = a.length/2;
+		if (right > left){
+			if (a.length % 2 == 1){
+				if (pivotPos == middle){
+					return (a[middle]);
+				} else {
+					if (pivotPos == middle){
+						if (middle == left){
+							return a[left];
+						} else {
+							return (a[middle] + minmax(a, left, middle - 1, 1)) / 2.0;
+						}
+					}
+					if (pivotPos == middle - 1){
+						if (middle == right){
+							return a[right];
+						} else {
+							return (a[middle - 1] + minmax(a, middle, a.length - 1, 0)) / 2.0;
+						}
+					}
+					if (pivotPos < middle){
+						return median(a, pivotPos + 1, right);
+					} else {
+						return (median(a, left, pivotPos - 1));
+					}
+				}
+			}
+		}
+		return a[middle];
+	}
+
+	public static int partition2(int[] a, int left, int right) {
+		int splitPos = left;
+		for (int i = left; i < right; i++) {
+			if (a[i] < a[right]) {
+				swap2(a, i, splitPos);
+				splitPos++;
+			}
+		}
+		swap2(a,splitPos,right);
+		return splitPos;
+	}
+
+	public static void swap2(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static int minmax(int[] a, int left, int right, int num){
+		int init = a[left];
+		if (num == 0){ //min
+			for (int i = 0; i <= right; i++){
+				if (a[i] < init){
+					init = a[i];
+				}
+			}
+		}
+		if (num == 1){ //max
+			for (int i = 0; i <= right; i++){
+				if (a[i] > init){
+					init = a[i];
+				}
+			}
+		}
+		return init;
+	}
+
+
 
 
 	public static int[] heapSort(int[] arr) {
