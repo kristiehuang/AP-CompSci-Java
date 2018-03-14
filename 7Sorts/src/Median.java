edb5f5fe1476f3d399c0d9129b3a61d423f4af37
Kristie Huang <kristiehuang01@gmail.com>
2018-03-14 16:09:51 -0700

    3/14 - FIXED selectionsort, broken Median, broken Heapsort

---
+++ 7Sorts/src/Median.java
@@ -0,0 +1,90 @@
+
+public class Median {
+	public static void main(String[] args) {
+		int[] array = { 3, 6, 9, 7, 8, 5 };
+
+		//fill array w numbers
+
+		median(array);
+	}
+	
+	
+	public static double median(int[] a) {
+		double sorted = medianQuick(a, 0, a.length - 1);
+		System.out.println(sorted);
+		return sorted;
+	}
+	
+	public static double medianQuick(int[] a, int left, int right) {
+		int pivotPos = partition2(a, left, right);
+		int middle = a.length/2;
+		if (right > left){
+			if (a.length % 2 == 1){
+				if (pivotPos == middle){
+					return (a[middle]);
+				} else {
+					if (pivotPos == middle){
+						if (middle == left){
+							return a[left];
+						} else {
+							return (a[middle] + minmax(a, left, middle - 1, 1)) / 2.0;
+						}
+					}
+					if (pivotPos == middle - 1){
+						if (middle == right){
+							return a[right];
+						} else {
+							return (a[middle - 1] + minmax(a, middle, a.length - 1, 0)) / 2.0;
+						}
+					}
+					if (pivotPos < middle){
+						return medianQuick(a, pivotPos + 1, right);
+					} else {
+						return medianQuick(a, left, pivotPos - 1);
+					}
+				}
+			}
+		}
+		return a[middle];
+	}
+
+	public static int partition2(int[] a, int left, int right) {
+		int splitPos = left;
+		for (int i = left; i < right; i++) {
+			if (a[i] < a[right]) {
+				swap2(a, i, splitPos);
+				splitPos++;
+			}
+		}
+		swap2(a,splitPos,right);
+		return splitPos;
+	}
+
+	public static void swap2(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	public static int minmax(int[] a, int left, int right, int num){
+		int init = a[left];
+		if (num == 0){ //min
+			for (int i = 0; i <= right; i++){
+				if (a[i] < init){
+					init = a[i];
+				}
+			}
+		}
+		if (num == 1){ //max
+			for (int i = 0; i <= right; i++){
+				if (a[i] > init){
+					init = a[i];
+				}
+			}
+		}
+		return init;
+	}
+
+
+
+}
--- 7Sorts/src/Sorts.java
+++ 7Sorts/src/Sorts.java
@@ -4,22 +4,16 @@ public class Sorts {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] array1 = { 4, 5, 1, 3, 2 };
-
-		int[] array2 = { 4, 5, 1, 3, 2 };
-
-		int[] array3 = { 4, 5, 1, 3, 2 };
-
+		int[] array1 = { 2, 32, 91, 4 };
+		int[] array2 = { 2, 32, 91, 4 };
+		int[] array3 = { 3, 5, 2, 4 };
 		int[] array4 = { 3, 6, 1, 4, 2, 10, 7, 9, 8, 5 };
 
-		//fill array w numbers
 
 		bubbleSort(array1);
 		insertSort(array2);
 		selectionSort(array3);
 		heapSort(array4);
-
-		//median(array4);
 
 	}
 
@@ -43,15 +37,11 @@ public class Sorts {
 						if (arr[i] > arr[i + 1]) { 
 							arr[i] = arr[i + 1];
 							arr[i + 1] = dis;
-						}
-					}
-				}
-			}
+						}}}}
 		}
 
 		System.out.println("bubble");
 		toPrint(arr);
-
 		return arr;
 	}
 
@@ -66,44 +56,27 @@ public class Sorts {
 					if (nextInt > toSort) {
 						arr[i - 1] = toSort;
 						arr[i] = nextInt;
-					}
-				}
-			}
-		}
+					}}}}
 
 		System.out.println("insertion");
 		toPrint(arr);
-
 		return arr;
 	}
-
 
 	public static int[] selectionSort(int[] arr) { //iterate to find and store the shortest, then swap with first
 
 		if (arr != null) {
-			for(int a = 0; a < arr.length; a++) {
-				int thisInt = arr[a];
-
-				int swapPosition = 0;
-				System.out.println("a" + a);
-
-				//a = 3
-				//i = 4
-				//fix this DOES THE I FOR LOOP RUN AT ALL
+			for(int a = 0; a < arr.length - 1; a++) {
+				int min = a;
 
 				for(int i = a + 1; i < arr.length; i++) {
-					int nextInt = arr[i];
 
-					if (thisInt > nextInt) {
-
-						thisInt = nextInt;
-						System.out.println("i" + i);
-						swapPosition = i;
+					if (arr[min] > arr[i]) {
+						min = i;
 					}           
 				}
-
-
-				arr[swapPosition] = arr[a];
+				int thisInt = arr[min];
+				arr[min] = arr[a];
 				arr[a] = thisInt;
 			}
 		}
@@ -114,117 +87,6 @@ public class Sorts {
 		return arr;
 
 	}
-	//	// QUICK SORT
-
-
-	public void quicksort(int[] a) {
-		quick(a, 0, a.length-1);
-	}
-
-	public void quick(int[] a, int left, int right) {
-		if (right > left) { //if last index is bigger than 0
-			int pivotPos = partition(a, left, right);
-
-			quick(a, left, pivotPos-1);
-			quick(a, pivotPos+1, right);
-		}
-	}
-
-	public int partition(int[] a, int left, int right) {
-		int splitPos = left; //1
-		for (int i = left; i < right; i++) { //index from 0
-			if (a[i] < a[right]) {
-				swap(a, i, splitPos);
-				splitPos++;
-			}
-		}
-		swap(a,splitPos,right);
-		return splitPos;
-	}
-
-	public void swap(int[] a, int i, int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-	}
-
-
-
-	public static double median(int[] a) {
-		return median(a, 0, a.length-1);
-	}
-
-	public static double median(int[] a, int left, int right) {
-		int pivotPos = partition2(a, left, right);
-		int middle = a.length/2;
-		if (right > left){
-			if (a.length % 2 == 1){
-				if (pivotPos == middle){
-					return (a[middle]);
-				} else {
-					if (pivotPos == middle){
-						if (middle == left){
-							return a[left];
-						} else {
-							return (a[middle] + minmax(a, left, middle - 1, 1)) / 2.0;
-						}
-					}
-					if (pivotPos == middle - 1){
-						if (middle == right){
-							return a[right];
-						} else {
-							return (a[middle - 1] + minmax(a, middle, a.length - 1, 0)) / 2.0;
-						}
-					}
-					if (pivotPos < middle){
-						return median(a, pivotPos + 1, right);
-					} else {
-						return (median(a, left, pivotPos - 1));
-					}
-				}
-			}
-		}
-		return a[middle];
-	}
-
-	public static int partition2(int[] a, int left, int right) {
-		int splitPos = left;
-		for (int i = left; i < right; i++) {
-			if (a[i] < a[right]) {
-				swap2(a, i, splitPos);
-				splitPos++;
-			}
-		}
-		swap2(a,splitPos,right);
-		return splitPos;
-	}
-
-	public static void swap2(int[] a, int i, int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-	}
-
-	public static int minmax(int[] a, int left, int right, int num){
-		int init = a[left];
-		if (num == 0){ //min
-			for (int i = 0; i <= right; i++){
-				if (a[i] < init){
-					init = a[i];
-				}
-			}
-		}
-		if (num == 1){ //max
-			for (int i = 0; i <= right; i++){
-				if (a[i] > init){
-					init = a[i];
-				}
-			}
-		}
-		return init;
-	}
-
-
 
 
 	public static int[] heapSort(int[] arr) {
