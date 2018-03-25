805920961a4575dbd6a2b8951626080b781725c0
Kristie Huang <kristiehuang01@gmail.com>
2018-03-25 10:45:28 -0700

    fixeD MERGE SORT FINALLY omL (heapsort still broken)

--- 7Sorts/src/MergeSort.java
+++ 7Sorts/src/MergeSort.java
@@ -6,6 +6,8 @@ public class MergeSort {
 		int[] array = { 6, 5, 2, 1, 3 };
 
 		mergeSort(array);
+		
+		
 
 	}
 
@@ -27,21 +29,18 @@ public class MergeSort {
 
 
 	public static int[] ms(int[] a) {
-		int[] returnArr;
-
 		if (a.length == 1) {
-			returnArr = a;
+			return a;
 		}
 		else {
-			returnArr = merge((ms(partitionLeft(a))), (ms(partitionRight(a))));
+			return merge((ms(partitionLeft(a))), (ms(partitionRight(a))));
 			//sort left; sort right; merge in sorted order
 		}
 
-		return returnArr;
 	}
 
 	public static int[] merge(int[] left, int[] right) {
-		int totalLength = left.length + right.length;
+	    int totalLength = left.length + right.length;
 		int[] sorted = new int[totalLength];
 
 		if (left.length == 0) {
@@ -50,55 +49,26 @@ public class MergeSort {
 		else if (right.length == 0) {
 			return left;
 		}
+        else {
+            int l = 0;
+            int r = 0;
+            for(int i = 0; i < totalLength; i++) {
+			    if (r == right.length || (l != left.length && left[l] < right[r])) { //left smaller than right *
 
-		else if (left[0] < right[0]) {
-			sorted[0] = left[0];
-
-			for(int i = 1; i < totalLength; i++) {
-
-				for (int l = 1; l < left.length; l++) {
-					for (int r = 0; r < right.length; r++) {
-
-						if (left[l] < right[r]) { //left smaller than right
-							sorted[i] = left[l];
-						}
-						else { 
-							sorted[i] = right[r];
-						}
-
-					}
-
+					sorted[i] = left[l];
+					l++;
+				} else { 
+					sorted[i] = right[r];
+					r++;
 				}
 			}
 			return sorted;
-		}
-		else { //if right[0] < left[0] OR two values are same
-
-			sorted[0] = right[0];
-			for(int i = 1; i < totalLength; i++) {
-
-				for (int l = 0; l < left.length; l++) {
-					for (int r = 1; r < right.length; r++) {
-
-						if (left[l] < right[r]) { //left smaller than right
-							sorted[i] = left[l];
-						}
-						else { //right smaller than left
-							sorted[i] = right[r];
-						}
-
-					}
-
-				}
-			}
-			return sorted;
-		}
-
+        }
 	}
-
+	
 	public static int[] partitionLeft(int[] arr) {
-
-		int numElements = (arr.length / 2); //IF IT ROUNDS DOWN
+        
+		int numElements = (arr.length / 2);
 		int[] leftArr = new int[numElements];
 
 		for(int i = 0; i < numElements; i++) {
@@ -109,6 +79,7 @@ public class MergeSort {
 	}
 
 	public static int[] partitionRight(int[] arr) {
+
 		int numElements;
 		if (arr.length % 2 == 1) { //odd
 			numElements = (arr.length / 2) + 1;
@@ -117,11 +88,12 @@ public class MergeSort {
 			numElements = arr.length / 2;
 		}
 		int[] rightArr = new int[numElements];
-		for(int i = 0; i < numElements; i++) {
-			rightArr[i] = arr[numElements-1 + i];
+		for(int i = 0; i < numElements; i++) {			
+			rightArr[i] = arr[arr.length /2 + i];
 		}
 
 		return rightArr;
 	}
+
 
 }
